f3abadc871f5627eb0691b5a8e36d2d66ad2960b
joeyklee <j@jk-lee.com>
2018-06-18 14:38:47 -0400

    updated contentScript with TODOs

--- chrome-extension/save-resources-extension-oath/contentScript.js
+++ chrome-extension/save-resources-extension-oath/contentScript.js
@@ -3,14 +3,27 @@
 
 	// get the selection on a page
 	chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+		// Get Selected Text
 	  if (request.method == "getSelection") {
 	  	var mySelection = window.getSelection().toString();
 	  	console.log("getSelection says: ", mySelection);
 	    sendResponse({data: mySelection });
 	  }
-	  // else {
-	  //   sendResponse({data: ''}); 
+	  
+
+	  // TODO: Get Images
+	  // if( request.method == "getImages"){
+	  // 	let imgs = document.querySelectorAll("img");
 	  // }
+
+	  // TODO: Get Links & Refs
+	  // if( request.method == "getRefs"){
+	  // 	let imgs = document.querySelectorAll("a");
+	  // }
+
+
 	});
+
+
 
 })();
