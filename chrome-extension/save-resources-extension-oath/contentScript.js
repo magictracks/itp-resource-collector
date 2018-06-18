e09e8c717cfb5ecf837b013b45e9c6ab2f9fdf8a
joeyklee <j@jk-lee.com>
2018-06-18 13:09:16 -0400

    fixed if/else statement to send data properly

--- chrome-extension/save-resources-extension-oath/contentScript.js
+++ chrome-extension/save-resources-extension-oath/contentScript.js
@@ -3,10 +3,14 @@
 
 	// get the selection on a page
 	chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	  if (request.method == "getSelection")
-	    sendResponse({data: window.getSelection().toString()});
-	  else
-	    sendResponse({}); 
+	  if (request.method == "getSelection") {
+	  	var mySelection = window.getSelection().toString();
+	  	console.log("getSelection says: ", mySelection);
+	    sendResponse({data: mySelection });
+	  }
+	  // else {
+	  //   sendResponse({data: ''}); 
+	  // }
 	});
 
 })();
