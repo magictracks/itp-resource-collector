e7dd16a62caa890fd77125a802520f21ce7a68c0
joeyklee <j@jk-lee.com>
2018-06-27 14:54:29 -0400

    removed server directory

--- server/helpers/auth.js
+++
@@ -1,9 +0,0 @@
-var db = require('../models');
-
-exports.githubAuthRedirect = function(req, res){
-
-  res.redirect('/app');
-}
-
-
-module.exports = exports;
--- server/models/index.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-mongoose.set('debug', true);
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/itp-resource-collector', {
-  keepAlive: true,
-  reconnectTries: Number.MAX_VALUE
-});
-
-module.exports.User = require("./user");
