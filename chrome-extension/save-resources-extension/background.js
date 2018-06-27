903e510d01a1534800bf18d2f4010b55e0d73f35
joeyklee <j@jk-lee.com>
2018-06-27 18:19:14 -0400

    moved unused extensions to archive

--- chrome-extension/save-resources-extension/background.js
+++
@@ -1,9 +0,0 @@
-function getword(info,tab) {
-  console.log(info.selectionText);
-}
-
-chrome.contextMenus.create({
-  title: "Save Ressource: %s",
-  contexts:["selection"],
-  onclick: getword
-});
--- chrome-extension/save-resources-extension/selection.js
+++
@@ -1,6 +0,0 @@
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.method == "getSelection")
-    sendResponse({data: window.getSelection().toString()});
-  else
-    sendResponse({}); 
-});
