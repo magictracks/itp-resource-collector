d620d508931021973300a76a9f6e8bd882b9f7d4
joeyklee <j@jk-lee.com>
2018-08-10 17:56:35 -0400

    set up basic choo scaffold for browser extension

---
+++ browser-extension/contentScript.js
@@ -0,0 +1,25 @@
+(function(){
+	console.log("Hello from contentScript.js!");
+
+	// get the selection on a page
+	// chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+	// 	// Get Selected Text
+	//   if (request.method == "getSelection") {
+	//   	var mySelection = window.getSelection().toString();
+	//   	console.log("getSelection says: ", mySelection);
+	//     sendResponse({data: mySelection });
+	//   }
+	// });
+
+	chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+		// Get Selected Text
+	  if (request.method == "getWindowLocation") {
+	  	let url = window.location.href;
+	  	console.log("get location says: ", url);
+	    sendResponse({data: url });
+	  }
+	});
+
+	console.log("window location: ", window.location);
+
+})();
